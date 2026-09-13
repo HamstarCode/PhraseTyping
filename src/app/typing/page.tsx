04696238c4c88d7e89b78fceb781484e31845dbe
Hamstar <304941107+HamstarCode@users.noreply.github.com>
2026-09-13 19:27:09 +0900

    add typing score system

--- src/app/typing/page.tsx
+++ src/app/typing/page.tsx
@@ -29,6 +29,9 @@ const [currentTurn, setCurrentTurn] = useState(0);
 
 const result = currentTurn >= conversation.length;
 
+
+
+
 useEffect(() => {
   if (result) {//はじめに処理やから＋１後にブッチする
     return;
@@ -43,19 +46,29 @@ const typingFinished =
   !result &&
   currentCharIndex >= conversation[currentTurn].text.length;
 
+//正答率の処理
+
+const [mistakeCount, setMistakeCount] = useState(0);
+const [totalTypeCount, setTotalTypeCount] = useState(0);
+
+
+
 useEffect(() => {
   const handleKeyDown = (event: KeyboardEvent) => {
     if (event.key.length !== 1) return;
-
+    setTotalTypeCount(totalTypeCount + 1);
+    if (startTime === null) {
+      setStartTime(Date.now());
+    }
 
     if (
       event.key === conversation[currentTurn].text[currentCharIndex]
     ) {
       setCurrentCharIndex(currentCharIndex + 1);
     }
-
-
-
+    else {
+      setMistakeCount(mistakeCount + 1);
+    }
 
   };
 
@@ -75,8 +88,66 @@ useEffect(() => {
 }, [typingFinished]);
 
 
+
+
+// 正しく打った回数
+const correctTypeCount = totalTypeCount - mistakeCount;
+
+const accuracy =
+  totalTypeCount === 0
+    ? 100
+    : (correctTypeCount / totalTypeCount) * 100;
+
+
+
+
+
+
+
+
+
+// タイマーの処理
+const [startTime, setStartTime] = useState<number | null>(null);
+const [time, setTime] = useState(0);
+
+
+useEffect(() => {
+  if (startTime === null || result) return;
+
+  const timer = setInterval(() => {
+    setTime(Math.floor((Date.now() - startTime) / 1000));
+  }, 100);
+
+  return () => {
+    clearInterval(timer);
+  };
+}, [[startTime, result]]);
+
+
+//scoreの処理
+
+// 1秒あたりに正しく打った文字数
+const typingSpeed =
+  time > 0 ? correctTypeCount / time : 0;
+
+// 4文字/秒を100点として、Speedを0〜100点に変換
+const speedScore =
+  Math.min((typingSpeed / 4) * 100, 100);
+
+// Accuracy 60% + Speed 40%
+const score =
+  accuracy * 0.6 + speedScore * 0.4;
+
+// 最終スコアを整数に
+const finalScore = Math.round(score);
+
+
+
+
+
 return (
 <main>
+  <p>Time：{time} sec</p>
     {conversation.map((message, index) => (
       index <currentTurn && (
     <p 
@@ -92,8 +163,12 @@ return (
     ))}
     {result ? (
       <div className="result">
-        <h2>RESULT</h2>
-        <p>Typing completed!</p>
+      <h2>RESULT</h2>
+
+      <p>Time: {time}s</p>
+      <p>Accuracy: {accuracy.toFixed(2)}%</p>
+      <p>Speed: {typingSpeed.toFixed(2)} chars/s</p>
+      <p>Score: {finalScore}</p>
       </div>
     ) : (
       <div className="now-typing">
